1a17a33347033a0b6e396dd2ac6fae89f0b30581
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-08 16:51:43 +0100

    TabsResult

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,18 +7,8 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'HomePage',
-        loadChildren: () => import('../HomePage/tab1.module').then(m => m.Tab1PageModule)
-      },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
+      
+  
       {
         path: '',
         redirectTo: '/tabs/HomePage',
