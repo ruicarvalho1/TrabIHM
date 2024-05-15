a472de0890d6e951b8627e9a9399d320a89612ce
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-16 00:19:46 +0100

    Auth

--- src/app/entrar/entrar.page.ts
+++ src/app/entrar/entrar.page.ts
@@ -1,4 +1,9 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { LoadChildren } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.page.scss'],
 })
 export class EntrarPage implements OnInit {
+  credentials = this.fb.nonNullable.group({
+    email: ['rui.carvalho@ipvc.pt', Validators.required],
+    password: ['123456', Validators.required],
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get email() {
+    return this.credentials.controls.email;
   }
 
+  get password() {
+    return this.credentials.controls.password;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  async login() {}
 }
