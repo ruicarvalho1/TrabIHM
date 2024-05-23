8c2d283a8f2d8c70f36927125098e4a16a2b4f3e
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-23 11:22:33 +0100

    Swiper

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -16,8 +16,8 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
     IonicModule,
     HomePageRoutingModule,
     TranslateModule,
-  
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateService,TranslateModule } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  
 })
-export class HomePage{
+export class HomePage {
+  public swiper!: Swiper;
 
+  constructor(private translateService: TranslateService) {}
 
-
-  constructor(private translateService: TranslateService) { }
-
-  onchangeLanguage( e: any) {
-    this.translateService.use(e.target.value ? e.target.value: "en")
+  onchangeLanguage(e: any) {
+    this.translateService.use(e.target.value ? e.target.value : 'en');
   }
-
- 
-
 }
