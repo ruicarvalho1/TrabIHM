33b483c92ff310622f5707d66256ef77c07e24a6
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-24 18:13:48 +0100

    Ver Tarefas

--- src/app/disciplinas/disciplinas.page.ts
+++ src/app/disciplinas/disciplinas.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-disciplinas',
   templateUrl: './disciplinas.page.html',
   styleUrls: ['./disciplinas.page.scss'],
 })
-export class DisciplinasPage implements OnInit {
+export class DisciplinasPage {
+  tasksAndDisciplines: any[] = [];
+  disciplinas: any[] = [];
+  users: any = null;
+  user = this.authService.getCurrentUser();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(
+    private translateService: TranslateService,
+    private toastController: ToastController,
+    private authService: AuthService,
+    private data: DataService
+  ) {}
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { Swiper } from 'swiper';
+import { DataService } from './../services/data.service';
+import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { User } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +13,31 @@ import { Swiper } from 'swiper';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  users: any = null;
+  user = this.authService.getCurrentUser();
   public swiper!: Swiper;
+  tarefas: any[] = [];
 
-  constructor(private translateService: TranslateService) {}
+  constructor(
+    private translateService: TranslateService,
+    private toastController: ToastController,
+    private authService: AuthService,
+    private data: DataService
+  ) {}
 
   onchangeLanguage(e: any) {
     this.translateService.use(e.target.value ? e.target.value : 'en');
+  }
+
+  async ionViewWillEnter() {
+    const userId = this.authService.getCurrentUserId();
+    if (userId) {
+      this.tarefas = await this.data.getTarefasDoUsuario(userId);
+      this.users = await this.data.getUserById(userId);
+
+      console.log('group: ', this.users);
+    } else {
+      console.log('Nenhum usuário autenticado encontrado.');
+    }
   }
 }
