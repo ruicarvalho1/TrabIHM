8731ce670e485e820776140bd1e782373d7b1fdf
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-24 13:20:45 +0100

    Criar Tarefa

--- src/app/nova-tarefa/nova-tarefa.module.ts
+++ src/app/nova-tarefa/nova-tarefa.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { NovaTarefaPage } from './nova-tarefa.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    NovaTarefaPageRoutingModule
+    NovaTarefaPageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [NovaTarefaPage]
+  declarations: [NovaTarefaPage],
 })
 export class NovaTarefaPageModule {}
--- src/app/nova-tarefa/nova-tarefa.page.ts
+++ src/app/nova-tarefa/nova-tarefa.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../services/data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nova-tarefa',
   templateUrl: './nova-tarefa.page.html',
   styleUrls: ['./nova-tarefa.page.scss'],
 })
-export class NovaTarefaPage implements OnInit {
+export class NovaTarefaPage {
+  formularioTarefa: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private dataService: DataService) {
+    this.formularioTarefa = this.fb.group({
+      prioridade: [''],
+      concluida: [false],
+      tarefa: [''],
+      nome_tarefa: [''],
+      data_limite: [''],
+      imagem: [''],
+    });
   }
 
+  async criarTarefa() {
+    try {
+      const novaTarefa = this.formularioTarefa.value;
+      await this.dataService.createTarefa(novaTarefa);
+      console.log('Tarefa criada com sucesso!');
+    } catch (error) {
+      console.error('Erro ao criar tarefa:', error);
+    }
+  }
+
+  cancelar() {
+    console.log('Operação cancelada');
+  }
+
+  async guardar() {
+    try {
+      console.log('Tarefa criada e guardada com sucesso!');
+    } catch (error) {
+      console.error('Erro ao guardar a tarefa:', error);
+    }
+  }
 }
