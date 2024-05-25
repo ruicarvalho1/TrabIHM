c529e1eba9982bdb91c3dd4382a62b263b83703a
ruicarvalho1 <rui.carvalho@ipvc.pt>
2024-05-25 01:07:29 +0100

    Criar Tarefa com imagem

--- src/app/tarefa/tarefa.page.ts
+++ src/app/tarefa/tarefa.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../services/data.service';
+import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-tarefa',
   templateUrl: './tarefa.page.html',
   styleUrls: ['./tarefa.page.scss'],
 })
-export class TarefaPage implements OnInit {
+export class TarefaPage {
+  users: any = null;
+  user = this.authService.getCurrentUser();
+  tarefas: any[] = [];
 
-  constructor() { }
+  constructor(
+    private translateService: TranslateService,
+    private toastController: ToastController,
+    private authService: AuthService,
+    private data: DataService
+  ) {}
 
-  ngOnInit() {
+  async ionViewWillEnter() {
+    const userId = this.authService.getCurrentUserId();
+    if (userId) {
+      this.tarefas = await this.data.getTarefasDoUsuario(userId);
+      this.users = await this.data.getUserById(userId);
+    } else {
+      console.log('Nenhum usuário autenticado encontrado.');
+    }
   }
-
 }
